df7d15ccdd63bbfb695ef1b532dd37c58b68b2c0
ABDULL-44 <mohamedbasith965@gmail.com>
2024-05-20 15:59:26 +0530

    Updated a commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+
+        // Commend
+        // Hi it is me Abdull
         Software_Engineer obj01 = new Software_Engineer("THOR", 1050, "JAVA", "GAMMING", 1000);
         Software_Engineer obj02 = new Software_Engineer("TONY STARK",45,"PYTHON","GAMMING",25);
 
+        // I am an Avenger
         obj01.info();
         System.out.println();
         obj02.info();
